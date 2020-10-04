040f0c193416171890972f3a6e3aa995d898814a
kisstar <dwh.chn@foxmail.com>
2020-10-04 20:32:14 +0800

    feat(navbar): 根据滚轮滑动位置切换显示

--- src/components/navbar/index.ts
+++ src/components/navbar/index.ts
@@ -1,22 +1,45 @@
 import { PREFIX_CLS } from '../../config';
+import { throttle } from '../../lib/utils';
 import './index.scss';
 
-// 控制在移动端下菜单项的显示
-$(`.${PREFIX_CLS}-navbar-toggler`).click(function handleClick() {
-  const $navbarCollapse = $(`.${PREFIX_CLS}-navbar-collapse`);
-  const $body = $('body');
+(function IIFE() {
+  // 控制在移动端下菜单项的显示
+  $(`.${PREFIX_CLS}-navbar-toggler`).click(function handleClick() {
+    const $body = $('body');
+    const $navbarCollapse = $(`.${PREFIX_CLS}-navbar-collapse`);
+    const navbarCollapseElement = $navbarCollapse.get(0) as HTMLDivElement;
 
-  if (!$navbarCollapse.hasClass(`${PREFIX_CLS}-navbar-show`)) {
-    const height = Math.min($navbarCollapse.height(), document.documentElement.clientHeight);
-    $navbarCollapse.height(0);
-    $navbarCollapse.addClass(`${PREFIX_CLS}-navbar-show`);
-    $navbarCollapse.animate({ height }, 500);
-    $body.addClass('no-scroll'); // 防止页面滚动(滚动穿透)
-  } else {
-    $body.removeClass('no-scroll');
-    $navbarCollapse.animate({ height: 0 }, 500, function handleEffect() {
-      $navbarCollapse.removeClass(`${PREFIX_CLS}-navbar-show`);
-      ($navbarCollapse.get(0) as HTMLDivElement).style.height = '';
-    });
-  }
-});
+    if (!$navbarCollapse.hasClass(`${PREFIX_CLS}-navbar-show`)) {
+      const height = Math.min($navbarCollapse.height(), document.documentElement.clientHeight);
+      $navbarCollapse.height(0);
+      $navbarCollapse.addClass(`${PREFIX_CLS}-navbar-show`);
+      $navbarCollapse.animate({ height }, 500, () => {
+        navbarCollapseElement.style.height = '';
+      });
+      $body.addClass('no-scroll'); // 防止页面滚动(滚动穿透)
+    } else {
+      $body.removeClass('no-scroll');
+      $navbarCollapse.animate({ height: 0 }, 500, () => {
+        $navbarCollapse.removeClass(`${PREFIX_CLS}-navbar-show`);
+        navbarCollapseElement.style.height = '';
+      });
+    }
+  });
+
+  // 向下滚动隐藏，反向则显示
+  let oldTop = 0;
+  $(window).scroll(
+    throttle(function handleScroll() {
+      const scrollingElement = document.scrollingElement || document.body;
+      const newTop = scrollingElement.scrollTop;
+      const $navbar = $(`.${PREFIX_CLS}-navbar`);
+
+      if (newTop > oldTop && newTop > 64) {
+        $navbar.slideUp();
+      } else {
+        $navbar.slideDown();
+      }
+      oldTop = newTop;
+    }, 24),
+  );
+})();
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,11 +6,11 @@ import { AnyFunction, AnyObject } from './interface';
  * @param {number} [delay=0] The number of milliseconds to throttle invocations to
  * @returns {Function} Returns the new throttled function
  */
-export function throttle(method: AnyFunction, context: AnyObject, delay: number): AnyFunction {
+export function throttle(method: AnyFunction, delay = 0, context: AnyObject = {}): AnyFunction {
   let wait = false;
-  return function wrappedhrottle() {
+  return function wrappedThrottle() {
     if (!wait) {
-      method.apply(context);
+      method.call(context);
       wait = true;
       setTimeout(() => {
         wait = false;
@@ -19,4 +19,18 @@ export function throttle(method: AnyFunction, context: AnyObject, delay: number)
   };
 }
 
-export default { throttle };
+/**
+ * @param {Function} method  The function to debounce
+ * @param {Object} [context={}] The value to use as this when calling method
+ * @param {number} [delay=0] The number of milliseconds to debounce invocations to
+ * @returns {Function} Returns the new debounced function
+ */
+export function debounce(method: AnyFunction, delay = 0, context: AnyObject = {}): AnyFunction {
+  let tId = 0;
+  return function wrappedDebounce() {
+    clearTimeout(tId);
+    tId = window.setTimeout(() => {
+      method.call(context);
+    }, delay);
+  };
+}
