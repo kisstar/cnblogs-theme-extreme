7c63bf6cb2c0c1e3debd241559cb3a9afc2d049f
kisstar <dwh.chn@foxmail.com>
2020-10-08 16:40:11 +0800

    feat: 侧边导航栏添加个人信息

--- src/components/sidebar/index.ts
+++ src/components/sidebar/index.ts
@@ -1,15 +1,25 @@
-import { PREFIX_CLS, BRAND } from '../../config';
+import { PREFIX_CLS, BRAND, IS_POST } from '../../config';
 import './index.scss';
 
 const htmlTemplate = `
-<a class="${PREFIX_CLS}-sidebar-brand" href="#">
-  <img src="${BRAND.logo}" width="30" height="30">
-  ${BRAND.text}
-</a>`;
+  <a class="${PREFIX_CLS}-sidebar-brand" href="#">
+    <img src="${BRAND.logo}" width="30" height="30">
+    ${BRAND.text}
+  </a>`;
+const $navigator = $('#navigator');
+// const $navList = $('#navList');
+
+// 添加个人信息
+if (IS_POST) {
+  $('#author_profile').prependTo($navigator);
+} else {
+  $('#sidebar_news').prependTo($navigator);
+}
 
 // 向侧边栏添加 LOGO 和标题
-$(htmlTemplate).insertBefore($('#navList').children().first());
+$(htmlTemplate).prependTo($navigator);
+
 // 控制侧边栏的显示
-$('#navigator').click(function handleClick() {
+$navigator.click(function handleClick() {
   $('#home').toggleClass(`${PREFIX_CLS}-sidebar-open`);
 });
