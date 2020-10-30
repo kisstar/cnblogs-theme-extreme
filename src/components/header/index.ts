dd83742408a41336e4b69383dbe51414fcf8f58a
kisstar <dwh.chn@foxmail.com>
2020-10-30 11:10:43 +0800

    feat: 博文页中将公告中内的个人信息添加到头部

--- src/components/header/index.ts
+++ src/components/header/index.ts
@@ -7,9 +7,9 @@ const $topNav = $('#top_nav');
 if (IS_POST) {
   const $postInfo = $(`<div class="${PREFIX_CLS}-header-post-info"></div>`);
   $topNav.css({ display: 'none' });
-  $blogTitle.append(
-    $postInfo.append($('.postTitle')).append($('.postDesc')).append($('#BlogPostCategory')),
-  );
+  $blogTitle
+    .append($postInfo.append($('.postTitle')).append($('.postDesc')).append($('#BlogPostCategory')))
+    .append($('#profile_block').find('br').remove().end());
 } else {
   $blogTitle.append($('#zzk_search')).append($topNav);
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,16 @@
-const { pathname } = document.location;
+const { origin, pathname } = document.location;
 const rPost = /\/p\/\d+\.html/;
+const rLocal = /localhost/;
+
+export const IS_DEV = rLocal.test(origin);
+
+export const IS_POST = rPost.test(pathname) || pathname === '/post.html'; /* for development */
 
 export const PREFIX_CLS = 'kisstar';
 
+const IMAGE_HOST = !IS_DEV ? 'https://images.cnblogs.com' : '';
+
 export const BRAND = {
   text: "Kisstar's 博客",
-  logo: 'https://images.cnblogs.com/cnblogs_com/dongwanhong/1857964/o_201003115132logo.png',
+  logo: `${IMAGE_HOST}/cnblogs_com/dongwanhong/1857964/o_201003115132logo.png`,
 };
-
-export const IS_POST = rPost.test(pathname) || pathname === '/post.html'; /* for development */
